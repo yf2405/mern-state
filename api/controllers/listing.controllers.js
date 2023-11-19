76f306278e0f0c19497aa04226974db052d411cb
Yerson Felipe Correa <yersonstuardlittle062@gmail.com>
2023-11-19 12:30:28 -0500

    complete Create listing page functionality

--- api/controllers/listing.controllers.js
+++ api/controllers/listing.controllers.js
@@ -3,11 +3,10 @@ import Listing from "../model/listing.model.js";
 
 export const createListing = async (req, res, next) => {
     try {
-        const listing = Listing.create(req.body);
+        const listing = await Listing.create(req.body); // asegúrate de usar await aquí
         return res.status(201).json(listing);
     } catch (error) {
         next(error);
-    }  
+    }
 };
-
 
--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -5,4 +5,4 @@ const router = express.Router();
 
 
 router.post('/create', verifyToken, createListing);
-export default router;
+export default router; 

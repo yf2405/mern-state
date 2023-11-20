b781c7d230a132feab79b64e6a4ebbe71d7ffecc
Yerson Felipe Correa <yersonstuardlittle062@gmail.com>
2023-11-19 20:55:22 -0500

    create update listing api

--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -1,9 +1,10 @@
 import  express from "express";
-import { createListing, deleteListing  } from "../controllers/listing.controllers.js";
+import { createListing, deleteListing, updateListing  } from "../controllers/listing.controllers.js";
 import { verifyToken } from "../utils/verify.User.js";
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 export default router; 

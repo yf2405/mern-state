820ff4448122a6b8fd659fff5ce9ec7e6e48bb82
Yerson Felipe Correa <yersonstuardlittle062@gmail.com>
2023-12-06 22:03:50 -0500

    Add show more listings functional

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,7 +16,7 @@ export default function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  console.log(listings);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -50,9 +50,13 @@ export default function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/v1/listing/get?${searchQuery}`);
       const data = await res.json();
+      if(data.length>8){
+        setShowMore(true);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -106,6 +110,20 @@ export default function Search() {
     urlParams.set('order', sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onshowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/v1/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if(data.length> 9){
+      setShowMore(true);
+    }
+    setListings([...listings, data]);
   };
   return (
     <div className='flex flex-col md:flex-row'>
@@ -225,6 +243,16 @@ export default function Search() {
            listings.map((listing) => (
             <ListingItem key={listing._id} listing={listing} />
            ))}
+
+           {showMore &&(
+            <button onClick={() =>{
+              onshowMoreClick();
+            }}
+            className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show More
+            </button>
+           )}
         </div>
       </div>
     </div>
